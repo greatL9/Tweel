db6a770446824fe6abc54f730c02d6e939606910
Great Lucky <lgreat098@gmail.com>
2025-09-17 00:31:36 +0100

    add the post section of the feed section

--- src/app/components/Feed.tsx
+++ src/app/components/Feed.tsx
@@ -1,7 +1,28 @@
 import { SparklesIcon } from "@heroicons/react/24/outline";
 import Input from "./Input";
+import Post from "./Post";
 
 export default function Feed() {
+  const posts = [
+    {
+      id: "1",
+      name: "Great Lucky",
+      username: "great",
+      userImg: "/me.jpeg",
+      img: "https://images.unsplash.com/photo-1465146344425-f00d5f5c8f07?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8fDB8fHww",
+      text: "nice view!",
+      timestamp: "2 hours ago",
+    },
+    {
+      id: "2",
+      name: "Grace Samuel",
+      username: "grace",
+      userImg: "/me.jpeg",
+      img: "https://images.unsplash.com/photo-1469474968028-56623f02e42e?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D://images.unsplash.com/photo-1465146344425-f00d5f5c8f07?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8fDB8fHww",
+      text: "good view!",
+      timestamp: "2 days ago",
+    },
+  ];
   return (
     <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
       <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
@@ -11,6 +32,9 @@ export default function Feed() {
         </div>
       </div>
       <Input />
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
     </div>
   );
 }
---
+++ src/app/components/Post.tsx
@@ -0,0 +1,81 @@
+import {
+  ChartBarIcon,
+  ChatBubbleOvalLeftEllipsisIcon,
+  HeartIcon,
+  ShareIcon,
+  TrashIcon,
+} from "@heroicons/react/24/outline";
+import { EllipsisHorizontalCircleIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+
+interface PostProps {
+  post: {
+    id: string;
+    name: string;
+    username: string;
+    userImg: string;
+    img: string;
+    text: string;
+    timestamp: string;
+  };
+}
+
+export default function Post({ post }: PostProps) {
+  return (
+    <div className="flex p-3 cursor-pointer border-b border-gray-200">
+      <Image
+        src={post.userImg}
+        alt="user"
+        width="44"
+        height="44"
+        className="rounded-full w-11 h-11 mr-4 cursor-pointer hover:brightness-95"
+      />
+      <div>
+        <div className="flex items-center justify-between">
+          <div className="flex space-x-1 whitespace-nowrap items-center">
+            <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
+              {post.name}
+            </h4>
+            <span className="text-sm sm:text-[15px]">@{post.username} - </span>
+            <span className="text-sm sm:text-[15px] hover:underline">
+              {post.timestamp}
+            </span>
+          </div>
+          <EllipsisHorizontalCircleIcon className="h-10 hoverEffect w-10 hover:bg-purple-100 hover:text-purple-500 p-2" />
+        </div>
+        <p className="text-gray-800 text-[15px] sm:text-[16px] mb-4">
+          {post.text}
+        </p>
+        <Image
+          src={post.img}
+          alt="post"
+          width="800"
+          height="50"
+          className="rounded-2xl mr-2 w-full h-auto"
+        />
+        <div className="flex justify-between text-gray-500 p-2 mt-1">
+          <ChatBubbleOvalLeftEllipsisIcon
+            className="h-9 w-9 hoverEffect p-2 hover:bg-purple-100
+            hover:text-purple-500"
+          />
+          <TrashIcon
+            className="h-9 w-9 hoverEffect p-2 hover:bg-red-100
+            hover:text-red-600"
+          />
+          <HeartIcon
+            className="h-9 w-9 hoverEffect p-2 hover:bg-red-100
+            hover:text-red-600"
+          />
+          <ShareIcon
+            className="h-9 w-9 hoverEffect p-2 hover:bg-purple-100
+            hover:text-purple-500"
+          />
+          <ChartBarIcon
+            className="h-9 w-9 hoverEffect p-2 hover:bg-purple-100
+            hover:text-purple-500"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
